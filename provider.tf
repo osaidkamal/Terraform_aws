fa67ff161f7ddc3597f10756987f5dd4cbffe3d8
Osaid Kamal <52270720+osaidkamal@users.noreply.github.com>
2022-12-28 20:04:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIATVXS2FNPMSWSEC6W"
-  secret_key = "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"
+  access_key = "*****************"
+  secret_key = "*****************"
 }
